94bc271c3800c2a771a72b452b13c1ee0bf6ec70
Manvitha <vm3695@srmist.edu.in>
2026-02-18 09:22:41 +0530

    Coded UC2 to Render OOPS as Banner

---
+++ OOPSBannerApp.java
@@ -0,0 +1,16 @@
+/**
+ * OOPSBannerApp UC2 – Banner Display Application
+ * @author Developer
+ * @version 2.0
+ */
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        System.out.println("  *  " + " " + "   *   " + " " + " ******  " + " " + "  ******");
+        System.out.println(" * * " + " " + "  *  * " + " " + " **   ** " + " " + "  ***   ");
+        System.out.println("*   *" + " " + " *    *" + " " + " **    **" + " " + "    *** ");
+        System.out.println("*   *" + " " + " *    *" + " " + " **   ** " + " " + "     ***");
+        System.out.println("*   *" + " " + " *    *" + " " + " ******  " + " " + "     ***");
+        System.out.println(" * * " + " " + "  *  * " + " " + " **      " + " " + "    *** ");
+        System.out.println("  *  " + " " + "    *  " + " " + " **      " + " " + " *****  ");
+    }
+}
