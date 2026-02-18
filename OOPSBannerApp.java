d0e35365e822e99baa41dbbacfc512631aa7b802
Manvitha <vm3695@srmist.edu.in>
2026-02-18 08:58:06 +0530

    Coded UC1 to Print OOPS to Console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,10 @@
+/**
+ * OOPSBannerApp UC1 – OOPS Banner Display Application
+ * @author Developer
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
